e5857c617b7ce87ad89a4994d95d374a60839c3e
PeterW <w64935656@gmail.com>
2022-05-07 01:29:08 +0800

    Finsih Location and Weather related function

--- backend/routes/locationRoutes.js
+++ backend/routes/locationRoutes.js
@@ -3,7 +3,8 @@ const mongoose = require("mongoose");
 let Location = require("../models/locationModel");
 const fetch = require("node-fetch");
 
-router.get("/location/:location", (req, res) => {
+//Create Location Data
+router.get("/location/create/:location", (req, res) => {
   let location = req.params["location"];
   fetch(
     "http://api.weatherapi.com/v1/current.json?key=b6aad25f604944e0987142747221004&q=" +
@@ -26,6 +27,31 @@ router.get("/location/:location", (req, res) => {
         }
       );
     });
+});
+
+//FindALL
+router.get("/location", (req, res) => {
+  Location.find((err, results) => {
+    if (err) {
+      res.status(500).send({ msg: err.message });
+    } else {
+      res.send(results);
+    }
+  });
+});
+
+//Delete
+router.get("/location/delete/:location", (req, res) => {
+  let location = req.params["location"];
+  Location.findOne({ locationName: location }, (err, results) => {
+    if (err) {
+      res.status(500).send({ msg: err.message });
+    } else if (!results) {
+      res.send({ msg: "This location cannot find" });
+    } else {
+      res.send({ msg: "Location is deleted" });
+    }
+  }).remove();
 });
 
 module.exports = router;
--- backend/routes/weatherRoutes.js
+++ backend/routes/weatherRoutes.js
@@ -4,11 +4,11 @@ let Weather = require("../models/weatherModel");
 let Location = require("../models/locationModel");
 const fetch = require("node-fetch");
 
-router.get("/weather/:location", (req, res) => {
-  let location = req.params["location"];
+//Create Weather data
+router.get("/weather/create/:location", (req, res) => {
   fetch(
     "http://api.weatherapi.com/v1/current.json?key=b6aad25f604944e0987142747221004&q=" +
-      location
+      req.params["location"]
   )
     .then((res) => res.json())
     .then((data) => {
@@ -39,6 +39,74 @@ router.get("/weather/:location", (req, res) => {
           );
         }
       });
+    });
+});
+
+//FindAll
+router.get("/weather", (req, res) => {
+  Weather.find()
+    .populate("location")
+    .exec(function (err, result) {
+      if (err) {
+        res.status(500).send({ msg: err.message });
+      } else {
+        res.send(result);
+      }
+    });
+});
+
+//FindOne
+router.get("/weather/:location", (req, res) => {
+  Weather.find()
+    .populate("location")
+    .exec(function (err, results) {
+      if (err) {
+        res.status(500).send({ msg: err.message });
+      } else {
+        for (let i = 0; i < results.length; i++) {
+          if (results[i].location.locationName === req.params["location"]) {
+            res.send(results[i]);
+          }
+        }
+      }
+    });
+});
+
+//Update Weather data
+router.get("/weather/update/:location", (req, res) => {
+  Weather.find()
+    .populate("location")
+    .exec(function (err, results) {
+      if (err) {
+        res.status(500).send({ msg: err.message });
+      } else {
+        for (let i = 0; i < results.length; i++) {
+          if (results[i].location.locationName === req.params["location"]) {
+            Weather.findOne({ _id: results[i]._id }, (err, result) => {
+              if (err) {
+                res.status(500).send({ msg: err.message });
+              } else {
+                fetch(
+                  "http://api.weatherapi.com/v1/current.json?key=b6aad25f604944e0987142747221004&q=" +
+                    req.params["location"]
+                )
+                  .then((res) => res.json())
+                  .then((data) => {
+                    result.time = data.current.last_updated;
+                    result.temp_c = data.current.temp_c;
+                    result.wind_kph = data.current.wind_kph;
+                    result.wind_dir = data.current.wind_dir;
+                    result.humidity = data.current.humidity;
+                    result.precip_mm = data.current.precip_mm;
+                    result.vis_km = data.current.vis_km;
+                    result.save();
+                    res.send({ msg: "Location has been updated" });
+                  });
+              }
+            });
+          }
+        }
+      }
     });
 });
 
