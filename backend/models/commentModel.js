64d091bffc583431f2fa81c12c46725799614c88
PeterW <w64935656@gmail.com>
2022-05-06 20:25:41 +0800

    add commentModel

--- backend/models/commentModel.js
+++ backend/models/commentModel.js
@@ -1,9 +1,14 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const commentSchema = new Schema({
-
+  location: {
+    type: Schema.Types.ObjectId,
+    ref: "Location",
+    required: true,
+  },
+  content: { type: String, required: true },
 });
 
-const Comment = mongoose.model('Comment', commentSchema);
+const Comment = mongoose.model("Comment", commentSchema);
 module.exports = Comment;
--- backend/server.js
+++ backend/server.js
@@ -1,40 +1,40 @@
-const express = require('express');
-const cors = require('cors');
+const express = require("express");
+const cors = require("cors");
 const bodyParser = require("body-parser");
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-require('dotenv').config();
+require("dotenv").config();
 
 const app = express();
 const port = process.env.PORT || 8080;
 
 app.use(cors());
-app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(express.json());
 
 //connecting to Mongoose
 const uri = process.env.ATLAS_URI;
-mongoose.connect(uri,{useNewUrlParser:true, useUnifiedTopology: true});
+mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
 const connection = mongoose.connection;
 //connection failure
-connection.on('error', console.error.bind(console,'Connection error'));
+connection.on("error", console.error.bind(console, "Connection error"));
 //Connect Successfully
-connection.once('open',()=>{
-    console.log("MongoDB database connection established successfully");
-})
+connection.once("open", () => {
+  console.log("MongoDB database connection established successfully");
+});
 
 //import routes
-const login = require('./routes/loginRoutes')
-const register = require('./routes/registerRoutes')
-const admin = require('./routes/adminRoutes')
-const search = require('./routes/searchRoutes')
-app.use(login)
-app.use(register)
-app.use(admin)
-app.use(search)
+const login = require("./routes/loginRoutes");
+const register = require("./routes/registerRoutes");
+const admin = require("./routes/adminRoutes");
+const search = require("./routes/searchRoutes");
+app.use(login);
+app.use(register);
+app.use(admin);
+app.use(search);
 
-app.listen(port, ()=>{
-    console.log(`Server is running on port: ${port}`);
-})
+app.listen(port, () => {
+  console.log(`Server is running on port: ${port}`);
+});
