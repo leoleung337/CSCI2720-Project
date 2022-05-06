f5480a8f85cf74baa54c5b1a4b91c6046111281e
leoleung337 <leoleung337@gmail.com>
2022-05-06 15:07:37 +0800

    backend register user

--- backend/routes/registerRoutes.js
+++ backend/routes/registerRoutes.js
@@ -1,8 +1,26 @@
 const router = require('express').Router();
 const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs')
+const crypto = require('crypto');
+let User = require('../models/userModel');
 
-router.get("/register", (req, res) => {
-    res.send({msg:'Hello World. This is register routes'})
+router.post("/register", async(req, res) => {
+    //check existed username or email 
+    const userExisted = await User.findOne({username: req.body.username})
+    if (userExisted != null) return res.status(400).json({msg: "username existed."})
+
+    const hashedPassword = await bcrypt.hash(req.body.password, 10)
+    User.create({
+        _id: new mongoose.Types.ObjectId(),
+        username: req.body.username,
+        password: hashedPassword
+    }, (err, results) => {
+        if (err) {
+            res.status(400).send({msg: "User create error."})
+        } else{
+            res.status(200).send({msg: "User has been created."})
+        }
+    })
 })
 
 module.exports = router;
