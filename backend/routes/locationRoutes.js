cabbdfb62f3a181f5ef63624adfe34aab26315fa
PeterW <w64935656@gmail.com>
2022-05-09 02:20:57 +0800

    Add favourit location function

--- backend/routes/locationRoutes.js
+++ backend/routes/locationRoutes.js
@@ -1,6 +1,7 @@
 const router = require("express").Router();
 const mongoose = require("mongoose");
 let Location = require("../models/locationModel");
+let User = require("../models/userModel");
 const fetch = require("node-fetch");
 
 //Create Location Data
@@ -40,14 +41,45 @@ router.get("/location", (req, res) => {
   });
 });
 
+//Add favorite location
+router.get("/location/addFavorite/:location/:username", (req, res) => {
+  User.findOne({ username: req.params["username"] }, (err, user) => {
+    if (err) {
+      res.status(500).send({ msg: err.message });
+    } else if (!user) {
+      res.status(500).send({ msg: "This user cannot find" });
+    } else {
+      Location.findOne(
+        { locationName: req.params["location"] },
+        (err, location) => {
+          if (err) {
+            res.status(500).send({ msg: err.message });
+          } else if (!user) {
+            res.status(500).send({ msg: "This location cannot find" });
+          } else {
+            user.favouriteLocations.push(location._id);
+            let uniqueLocation = user.favouriteLocations.filter(
+              (element, index) => {
+                return user.favouriteLocations.indexOf(element) === index;
+              }
+            );
+            user.favouriteLocations = [...uniqueLocation];
+            user.save();
+            res.send({ msg: "User favourite location has been updated" });
+          }
+        }
+      );
+    }
+  });
+});
+
 //Delete
 router.get("/location/delete/:location", (req, res) => {
-  let location = req.params["location"];
-  Location.findOne({ locationName: location }, (err, results) => {
+  Location.findOne({ locationName: req.params["location"] }, (err, results) => {
     if (err) {
       res.status(500).send({ msg: err.message });
     } else if (!results) {
-      res.send({ msg: "This location cannot find" });
+      res.status(500).send({ msg: "This location cannot find" });
     } else {
       res.send({ msg: "Location is deleted" });
     }
