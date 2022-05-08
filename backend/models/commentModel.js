5e81e678a23a2cfa887e8ce7453c62b33d8ffcbf
Lwcxd <105037888+Lwcxd@users.noreply.github.com>
2022-05-08 19:43:01 +0800

    Update commentModel.js

--- backend/models/commentModel.js
+++ backend/models/commentModel.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const commentSchema = new Schema({
-  UserID: {type: String, required: true},
+  userName: {type: String, required: true},
   location: {
     type: Schema.Types.ObjectId,
     ref: "Location",
