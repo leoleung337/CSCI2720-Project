e4f690dc07cdcd3e093d244b2195a5b0103278e9
pm-ep <105105579+pm-ep@users.noreply.github.com>
2022-05-09 18:08:05 +0800

    Add files via upload
    User action #1: Table view

---
+++ client/src/Client/TableView.js
@@ -0,0 +1,33 @@
+//Table view of location (User action #1)
+
+import React from "react";
+
+class TableView extends React.Component {
+    constructor(props) {
+        super(props)
+    }
+    render() {
+        fetch("http://localhost:3000/weather")
+            .then(res => res.json())
+            .then(data => {
+                return (<><h1>TableView</h1>
+                    <table>
+                        <td>location</td><td>time</td><td>temp_c</td><td>wind_kph</td><td>wind_dir</td><td>humidity</td><td>precip_mm</td><td>vis_km</td>
+                        {data.map((weather, index) => <tr>
+                            <td><a href="http://localhost:8080/weather/${weather.location}">{weather.location}</a></td>
+                            <td>{weather.time}</td>
+                            <td>{weather.temp_c}</td>
+                            <td>{weather.wind_kph}</td>
+                            <td>{weather.wind_dir}</td>
+                            <td>{weather.humidity}</td>
+                            <td>{weather.precip_mm}</td>
+                            <td>{weather.vis_km}</td></tr>
+                        )}
+                    </table></>)
+            })
+    }
+}
+
+
+
+export default TableView
