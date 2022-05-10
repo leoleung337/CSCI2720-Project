e7de7f3ee3d000ef54e427dfaecb861e87f05338
hui4935 <100196311+hui4935@users.noreply.github.com>
2022-05-10 06:37:26 +0000

    add manage location

--- client/src/Admin/ManageLocation.js
+++ client/src/Admin/ManageLocation.js
@@ -1,19 +1,106 @@
-import React from "react";
+import React, { useEffect, useState } from 'react';
 
-class ManageLocation extends React.Component{
-    constructor(props){
+class ManageLocation extends React.Component {
+    constructor(props) {
         super(props)
     }
-    render(){
-        return(
-            <>
+    render() {
+        return (
             <div>
-                <h1>Manage Location</h1>
-
-
+                <CreateLocation />
+                <DeleteLocation />
+                <ReadLocation />
             </div>
-            </>
         );
     }
 }
-export default ManageLocation;
+
+class CreateLocation extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = { value: '' };
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleChange(event) {
+        this.setState({value: event.target.value});
+    }
+
+    handleSubmit(event) {
+        fetch("localhost:8080/location/create/" + this.state.value).then((res) => res.json());
+        event.preventDefault();
+    }
+
+    render() {
+        return (
+            <div>
+                <h2>Create</h2>
+                <form onSubmit={this.handleSubmit}>
+                    <label>
+                        Location:
+                        <input type="text" value={ this.state.value } onChange={ this.handleChange } />
+                    </label>
+                    <input type="submit" value="Submit" />
+                </form>
+            </div>
+        );
+    }
+}
+
+class DeleteLocation extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = { value: '' };
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleChange(event) {
+        this.setState({value: event.target.value});
+    }
+
+    handleSubmit(event) {
+        fetch("localhost:8080/location/delete/" + this.state.value).then((res) => res.json());
+        event.preventDefault();
+    }
+
+    render() {
+        return (
+            <div>
+                <h2>Delete</h2>
+                <form onSubmit={this.handleSubmit}>
+                    <label>
+                        Location:
+                        <input type="text" value={ this.state.value } onChange={ this.handleChange } />
+                    </label>
+                    <input type="submit" value="Submit" />
+                </form>
+            </div>
+        );
+    }
+}
+
+function ReadLocation(){
+    const [data, setData] = useState([]);
+    useEffect(()=>{
+        fetch('localhost:8080/location')
+            .then(response => response.json())
+            .then(setData);
+    }, []);
+
+    return (
+        <>
+            <div>
+                <h2>Read</h2>
+                <table>
+                    <tbody>
+                        { data.map((e) => <><tr><td>{e.location.name}</td><td>{e.location.lat}</td><td>{e.location.lon}</td></tr></>) }
+                    </tbody>
+                </table>
+            </div>
+        </>
+    );
+}
+
+export default ManageLocation
