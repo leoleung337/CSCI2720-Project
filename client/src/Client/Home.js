f1d36e1f70444d1c791c8cca8de76c734ece840b
leoleung337 <leoleung337@gmail.com>
2022-05-12 19:02:18 +0800

    Revert "Added u.a. 1-3 route"
    This reverts commit ca7dce4031b4fadc32feb820cc8abf114ad952b9.

--- client/src/Client/Home.js
+++ client/src/Client/Home.js
@@ -6,82 +6,73 @@ import React from "react";
 import Weather from "./Weather";
 import FavLoc from "./FavLoc";
 import "./home.css"
-import MapView from "./MapView";
-import TableView from "./TableView";
-import Search from "./Search";
 import EachLocation from "./EachLocation";
-import { Route, Routes, Link, useNavigate, Navigate, useParams } from 'react-router-dom';
+import {Route,Routes,Link,useNavigate,Navigate, useParams} from 'react-router-dom';
 import Cookies from "universal-cookie";
 import 'bootstrap/dist/css/bootstrap.css';
 import "bootstrap-icons/font/bootstrap-icons.css";
 
 
 
-class Home extends React.Component {
-    constructor(props) {
+class Home extends React.Component{
+    constructor(props){
         super(props)
     }
 
-    removeCookies = () => {
-        const c = new Cookies()
+    removeCookies =() =>{
+        const c= new Cookies()
         c.remove("username", { path: '/' })
         c.remove("role", { path: '/' })
         console.log('remove')
     }
-    render() {
-        const CallRoute = (props) => {
+    render(){
+        const CallRoute = (props)=>{
             let params = useParams().username
             const cookie = new Cookies()
-            let tokenusername = cookie.get('username')
-            if (!tokenusername) {
+            let tokenusername= cookie.get('username')
+            if (!tokenusername){
                 window.alert("please login first")
-                return (<Navigate to="/" />)
+                return(<Navigate to="/"/>)
             }
-            if (tokenusername != params) {
+            if (tokenusername!=params){
                 window.alert("You can't access other users' page")
-                return <Navigate to="/" />
+                return <Navigate to="/"/>
             }
-            return (
-                //navbar
-                <div className="container">
-                    <div style={{ paddingTop: 20 }} />
-                    <h2 style={{ fontSize: 45 }}> <Link to={`/user/${params}`}>Main Page</Link></h2>
-                    <h5>{params}</h5>
-                    {/*                 <div className="icon-bar" >  
+            return(
+            //navbar
+            <div className="container">
+                 <div style={{paddingTop:20}}/>
+                <h2 style={{fontSize:45}}> <Link to={`/user/${params}`}>Main Page</Link></h2>
+                <h5>{params}</h5>
+{/*                 <div className="icon-bar" >  
                     <a href="/" onClick={this.removeCookies}>Logout  <i class="bi bi-box-arrow-in-left"></i></a>
                     <a href={`/user/${params}/`}></a> 
                     <a href={`/user/${params}/`}></a> 
                     <a href={`/user/${params}/`}></a>
                 </div> */}
 
-                    <br />
-                    <div className="nav nav-tabs text-light" id="bar" >
-                        <Link to={`/user/${params}/TableView`} id="tableView">Table View</Link>
-                        <Link to={`/user/${params}/MapView`} id="mapView">Map View</Link>
-                        <Link to={`/user/${params}/Search`} id="search">Searching</Link>
-                        <Link to={`/user/${params}/FavouriteLocation`} id="favlocation">Favourite Locations</Link>
-                        <Link to="/" onClick={this.removeCookies} id="logout">Logout<i class="bi bi-box-arrow-in-left"></i></Link>
-                    </div>
-                    <br />
+                <br/>
+                <div className = "nav nav-tabs text-light" id="bar" >
+                    <Link to ={`/user/${params}/FavouriteLocation`} id= "favlocation">Favourite Locations</Link> 
+                    <Link to ="/" onClick={this.removeCookies} id="logout">Logout<i class="bi bi-box-arrow-in-left"></i></Link>
+                </div>
+                <br/>
 
-                    <Routes>
-                        <Route path="/*" element={<Weather username={params} />} />
-                        <Route path="/TableView" element={<TableView username={params} />} />
-                        <Route path="/MapView" element={<MapView username={params} />} />
-                        <Route path="/Search" element={<Search username={params} />} />
-                        <Route path="/FavouriteLocation" element={<FavLoc username={params} />} />
-                        <Route path="/:location" element={<EachLocation username={params} />} />
-                    </Routes>
+                <Routes>
+                    <Route path = "/*" element= {<Weather username={params}/>}/>
+                    <Route path = "/FavouriteLocation" element= {<FavLoc username={params}/>}/>
+                    <Route path = "/:location" element={<EachLocation username={params} />}/>
+                </Routes>
                 </div>
             )
         }
-        return (
-            <CallRoute />
+        return(
+            <CallRoute />  
         );
     }
 }
 
 
-
+        
 
 export default Home
