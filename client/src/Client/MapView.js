13aecbc517c72def2ec2177cbdd123976c97d776
pm-ep <105105579+pm-ep@users.noreply.github.com>
2022-05-13 04:45:31 +0800

    Update MapView.js

--- client/src/Client/MapView.js
+++ client/src/Client/MapView.js
@@ -1,46 +1,143 @@
 //Map view of location (User action #2)
 
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { Route, Routes, Link, useParams } from 'react-router-dom';
+import { GoogleMap, useLoadScript, Marker} from '@react-google-maps/api';
+import EachLocation from "./EachLocation";
 
-class MapView extends React.Component {
-    constructor(props) {
-        super(props)
-    }
-    render() {
-        fetch("http://localhost:3000/weather")
-        .then(res => res.json())
-        .then(data => {
-        return <>
-            <h1>MapView</h1>
-            <div id="map" style="width:100%;height:600px;"></div>
-            <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBUkPIk0dlSSf6s0w7WGkSLUmmiozaFcBQ&callback=mapCB">
-                {
-                function mapCB() {
-                    var mapProp = {
-                        center: new google.maps.LatLng(0, 0),
-                        zoom: 5,
-                    };
-                    var map = new google.maps.Map(document.getElementById("map"), mapProp);
-                    data.map((weather, index) => {
-                    var marker = new google.maps.Marker({ 
-                        position: {Lat:weather.location.latitude,Lng:weather.location.longitude},
-                        map:map,
-                    });
-                    var infoWin=new google.maos.InfoWindow({
-                        content:<><Link to='/:location'>{weather.location.locationName}</Link>
-                        <Routes><Route path="/:location" element={<EachLocation username={params} />} />
-                        </Routes></>                        
-                    })
-                    marker.addListener("click",()=>{infoWin.open(map,marker)})
-                    })
+const libraries = ["places"];
+const mapContainerStyle = {
+    width: "100%",
+    height: "70vh",
+    textAlign: "center"
+};
 
-                    marker.setMap(map);
-                }
-                }
-            </script>
-        </>;
-        })
-    }
+export default function MapView() {
+
+
+
+
+    const { isLoaded, loadError } = useLoadScript({
+        googleMapsApiKey: "AIzaSyB91UVLhKv--pecJHLAMwS2JpmGgYbsTps",
+    });
+
+    if (!isLoaded) return <div>Loading</div>;
+    return <Map />;
+
 }
 
-export default MapView
+function Map() {
+    const centre = {
+        lat: 0,
+        lng: 0
+    };
+
+    const [arr, setarr] = useState([]);
+
+    useEffect(() => {
+        const fet = async () => {
+            const response = await fetch(`http://localhost:8080/weather`);
+            const res = await response.json();
+            setarr(res);
+        };
+        fet();
+    }, []);
+
+    return (<><h1>Map View</h1>
+        <GoogleMap zoom={2} center={centre}>
+            {arr.map((info, index) => {
+                const marker = { lat: info.location.latitude, lng: info.location.longitude };
+                <Marker position={marker} />
+            })}
+        </GoogleMap></>
+    )
+
+}
+
+
+
+
+
+
+/*
+    const ref = useRef(null);
+    const [map, setMap] = useState();
+    
+    useEffect(() => {
+      if (ref.current && !map) {
+        setMap(new window.google.maps.Map(ref.current, {}));
+      }
+    }, [ref, map]);
+    
+    const { isLoaded, loadError } = useLoadScript({
+        googleMapsApiKey: "AIzaSyB91UVLhKv--pecJHLAMwS2JpmGgYbsTps",
+        libraries,
+    });
+    if (loadError) return "error loading map";
+    if (!isLoaded) return "loading map";
+
+
+    const Marker = (options) => {
+        const [marker, setMarker] = useState();
+
+        useEffect(() => {
+            if (!marker) {
+                setMarker(new google.maps.Marker());
+            }
+
+            // remove marker from map on unmount
+            return () => {
+                if (marker) {
+                    marker.setMap(null);
+                }
+            };
+        }, [marker]);
+        useEffect(() => {
+            if (marker) {
+                marker.setOptions(options);
+            }
+        }, [marker, options]);
+        return null;
+    };
+
+    return (
+        <div>
+            <h2>Map View</h2>
+            <GoogleMap
+                mapContainerStyle={mapContainerStyle}
+                zoom={2}
+                center={centre} id="map" />
+
+            <Wrapper apiKey={googleMapsApiKey}>
+                <Map center={centre}>
+                    {arr.map((info, index) => {
+                        const place = {
+                            lat: info.location.lat,
+                            lng: info.location.lng
+                        };
+                        <Marker position={place} />
+                    })
+                    }
+                </Map>
+            </Wrapper>
+
+            {/*(map,google)=>{
+                var marker = new google.maps.Marker({ 
+                    position: place,
+                    map:map,
+                });
+                var infoWin=new google.maos.InfoWindow({
+                content:<p>{info.location.locationName}</p>
+                     
+                    })
+                    marker.addListener("click",()=>{infoWin.open(map, marker)})
+                }
+
+
+
+
+
+        </div>
+
+    );
+}*/
