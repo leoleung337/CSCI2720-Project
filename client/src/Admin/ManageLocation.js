b83d0387b0aa3e62c0fea6209861cf01980b0d0f
hui4935 <100196311+hui4935@users.noreply.github.com>
2022-05-13 14:52:22 +0000

    add drop down menu

--- client/src/Admin/ManageLocation.js
+++ client/src/Admin/ManageLocation.js
@@ -21,20 +21,15 @@ class ManageLocation extends React.Component {
 class CreateLocation extends React.Component {
     constructor(props) {
         super(props);
-        this.state = { value: '' };
-        this.handleChange = this.handleChange.bind(this);
         this.handleSubmit = this.handleSubmit.bind(this);
     }
 
-    handleChange(event) {
-        this.setState({ value: event.target.value });
-    }
-
     handleSubmit(event) {
-        fetch("http://localhost:8080/admin/location/create/" + this.state.value, {
+        const location = document.getElementById('location').options[document.getElementById('location').selectedIndex].value;
+        fetch("http://localhost:8080/admin/location/create/" + location, {
             method: "POST",
             headers: { 'Content-Type': 'application/json' },
-            body: JSON.stringify({ location: this.state.value })
+            body: JSON.stringify({ location: location })
         }).then(response =>{
             response.json().then(df=>{
                 window.alert(df.msg)
@@ -51,7 +46,33 @@ class CreateLocation extends React.Component {
                     <label>
                         Location:
                         <br></br>
-                        <input type="text" value={ this.state.value } onChange={ this.handleChange } />
+                        <select name="location" id="location">
+                            <option value="Atlanta">Atlanta</option>
+                            <option value="BangKok">BangKok</option>
+                            <option value="Beijing">Beijing</option>
+                            <option value="Canberra">Canberra</option>
+                            <option value="Chicago">Chicago</option>
+                            <option value="Fukuoka-Shi">Fukuoka-Shi</option>
+                            <option value="Hakodate">Hakodate</option>
+                            <option value="Hong Kong">Hong Kong</option>
+                            <option value="Istanbul">Istanbul</option>
+                            <option value="Las Vegas">Las Vegas</option>
+                            <option value="London">London</option>
+                            <option value="Los Angeles">Los Angeles</option>
+                            <option value="Manila">Manila</option>
+                            <option value="Mexico City">Mexico City</option>
+                            <option value="Moscow">Moscow</option>
+                            <option value="Nagoya-Shi">Nagoya-Shi</option>
+                            <option value="Osaka-Shi">Osaka-Shi</option>
+                            <option value="Rio De Janeiro">Rio De Janeiro</option>
+                            <option value="Roman">Roman</option>
+                            <option value="Seoul">Seoul</option>
+                            <option value="Taiwan">Taiwan</option>
+                            <option value="Tokyo">Tokyo</option>
+                            <option value="Toronto">Toronto</option>
+                            <option value="Vancouver">Vancouver</option>
+                            <option value="Washington">Washington</option>
+                        </select>
                     </label>
                     <br></br>
                     <br></br>
