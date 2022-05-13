98dd3a6340fd86f0261502c099c3bfe39707ddf7
leoleung337 <leoleung337@gmail.com>
2022-05-13 12:55:57 +0800

    update

--- client/src/Client/TableView.js
+++ client/src/Client/TableView.js
@@ -4,46 +4,46 @@ import React, { useEffect, useState } from "react";
 import EachLocation from "./EachLocation";
 import { Route, Routes, Link, useParams } from 'react-router-dom';
 
-export default function TableView() {
-    const [arr, setarr] = useState([]);
-    const params=useParams().username;
-
-    useEffect(() => {
-        const fet = async () => {
-            const response = await fetch(`http://localhost:8080/weather`);
-            const res = await response.json();
-            setarr(res)
-
-        };
-        fet();
-    }, []);
-
-    let sorting=function() {
-        let sortby = document.querySelector("#sort").value;
-        if (sortby == 0) return;
-        let s = [],t=[],s_sort=[];
-        let tab=document.querySelector("#tab");
-        for (let i = 1; i < tab.children.length; i++) {
-            let row = []            
-            for (let j = 0; j < 7; j++) {
-            row[j] = tab.children[i].children[j].innerText;
-            }
-            t[i-1]=row
-            s[i-1] = parseFloat(tab.children[i].children[sortby].innerText);
-            s_sort[i-1] = parseFloat(tab.children[i].children[sortby].innerText);
-        
-    }
-        s_sort.sort((x, y) => x - y);
-
-        for (let i = 1; i < tab.children.length; i++) {
-            let newIndex = s.indexOf(s_sort[i]);
-            for (let j=0;j<7;j++){
-                tab.children[i].children[j].innerText=t[newIndex][j];
-
-            }
-            s[newIndex]=9999999
+    export default function TableView() {
+        const [arr, setarr] = useState([]);
+        const params=useParams().username;
+    
+        useEffect(() => {
+            const fet = async () => {
+                const response = await fetch(`http://localhost:8080/weather`);
+                const res = await response.json();
+                setarr(res)
+    
+            };
+            fet();
+        }, []);
+    
+        let sorting=function() {
+            let sortby = document.querySelector("#sort").value;
+            if (sortby == 0) return;
+            let s = [],t=[],s_sort=[];
+            let tab=document.querySelector("#tab");
+            for (let i = 1; i < tab.children.length; i++) {
+                let row = []            
+                for (let j = 0; j < 8; j++) {
+                row[j] = tab.children[i].children[j].innerText;
+                }
+                t[i-1]=row
+                s[i-1] = parseFloat(tab.children[i].children[sortby].innerText);
+                s_sort[i-1] = parseFloat(tab.children[i].children[sortby].innerText);
+            
         }
-    }
+            s_sort.sort((x, y) => x - y);
+    
+            for (let i = 1; i < tab.children.length; i++) {
+                let newIndex = s.indexOf(s_sort[i]);
+                for (let j=0;j<8;j++){
+                    tab.children[i].children[j].innerText=t[newIndex][j];
+    
+                }
+                s[newIndex]=9999999
+            }
+        }
     return (
         <>
             <h1>TableView</h1>
@@ -63,7 +63,7 @@ export default function TableView() {
 
 
             <table id="tab">
-                <tr><td>location</td><td>temp_c</td><td>wind_kph</td><td>wind_dir</td><td>humidity</td><td>precip_mm</td><td>vis_km</td></tr>
+                <tr><td>location</td><td>temp_c</td><td>wind_kph</td><td>wind_dir</td><td>humidity</td><td>precip_mm</td><td>vis_km</td><td>last updated time</td></tr>
                 {arr.map((weather, index) =>
                     <tr id={index}>
                         <td><Link to ={`/user/${params}/${weather.location.locationName}`}>{weather.location.locationName}</Link></td>
@@ -72,7 +72,9 @@ export default function TableView() {
                         <td>{weather.wind_dir}</td>
                         <td>{weather.humidity}</td>
                         <td>{weather.precip_mm}</td>
-                        <td>{weather.vis_km}</td></tr>
+                        <td>{weather.vis_km}</td>
+                        <td>{weather.time}</td></tr>
+
                 )}
 
             </table>
