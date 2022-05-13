8eab2ac2cdece465bf368bb7f09245822fc231bc
pm-ep <105105579+pm-ep@users.noreply.github.com>
2022-05-14 04:44:38 +0800

    optimised some codes

--- client/src/Client/TableView.js
+++ client/src/Client/TableView.js
@@ -21,78 +21,69 @@ export default function TableView() {
     let sorting = function () {
         let sortby = document.querySelector("#sort").value;
         if (sortby == 0) return;
-        let s = [], t = [], s_sort = [];
+        let s = [], s_sort = [];
         let tab = document.querySelector("#tab");
         for (let i = 1; i < tab.children.length; i++) {
-            let row = []
-            for (let j = 0; j < 8; j++) {
-                row[j] = tab.children[i].children[j].innerText;
-            }
-            t[i - 1] = row
             s[i - 1] = parseFloat(tab.children[i].children[sortby].innerText);
             s_sort[i - 1] = parseFloat(tab.children[i].children[sortby].innerText);
-
         }
+
         if (document.querySelector("#order").value == 0)
             s_sort.sort((x, y) => x - y);
         else s_sort.sort((x, y) => y - x);
-        
-        let index=[-1]
+
+        let index = [-1]
         for (let i = 1; i < tab.children.length; i++) {
             index[i] = s.indexOf(s_sort[i - 1]) + 1 //The i-th element is the index-i th chiled of tab;
-            s[index[i]-1] = 9999999
+            s[index[i] - 1] = 9999999
         }
 
         for (let i = 1; i < tab.children.length; i++) {
             let loc = tab.children[index[i]]
-            tab.appendChild(loc)   
-            for (let k=0;k<index.length;k++){
-                if (index[k]>index[i]) index[k]--;
+            tab.appendChild(loc)
+            for (let k = 0; k < index.length; k++) {
+                if (index[k] > index[i]) index[k]--;
             }
-          
+
         }
     }
 
     return (<>
-            <h2>Table View</h2>
-            <form>
-                <label style={{ paddingTop: 30 }}>Sort by:</label>
-                <select id="sort">
-                    <option value="0">Default</option>
-                    <option value="1">temp_c</option>
-                    <option value="2">wind_kph</option>
-                    <option value="4">humidity</option>
-                    <option value="5">precip_mm</option>
-                    <option value="6">vis_km</option>
-                </select>
-                <label style={{ paddingTop: 30 }}>Order:</label>
-                <select id="order">
-                    <option value="0">ascending</option>
-                    <option value="1">descending</option>
-                </select>
-                <button type="button" id="sort" onClick={() => sorting()}>enter</button>
-            </form>
+        <h2>Table View</h2>
+        <form>
+            <label style={{ paddingTop: 30 }}>Sort by:</label>
+            <select id="sort">
+                <option value="0">Default</option>
+                <option value="1">temp_c</option>
+                <option value="2">wind_kph</option>
+                <option value="4">humidity</option>
+                <option value="5">precip_mm</option>
+                <option value="6">vis_km</option>
+            </select>
+            <label style={{ paddingTop: 30 }}>Order:</label>
+            <select id="order">
+                <option value="0">ascending</option>
+                <option value="1">descending</option>
+            </select>
+            <button type="button" id="sort" onClick={() => sorting()}>enter</button>
+        </form>
 
-            <div style={{ paddingTop: 30 }} />
-            <table id="tab">
-                <tr><td>location</td><td>temp_c</td><td>wind_kph</td><td>wind_dir</td><td>humidity</td><td>precip_mm</td><td>vis_km</td><td>last updated time</td></tr>
-                {arr.map((weather, index) =>
-                    <tr id={index}>
-                        <td><Link to={`/user/${params}/${weather.location.locationName}`}>{weather.location.locationName}</Link></td>
-                        <td>{weather.temp_c}</td>
-                        <td>{weather.wind_kph}</td>
-                        <td>{weather.wind_dir}</td>
-                        <td>{weather.humidity}</td>
-                        <td>{weather.precip_mm}</td>
-                        <td>{weather.vis_km}</td>
-                        <td>{weather.time}</td></tr>
+        <div style={{ paddingTop: 30 }} />
+        <table id="tab">
+            <tr><td>location</td><td>temp_c</td><td>wind_kph</td><td>wind_dir</td><td>humidity</td><td>precip_mm</td><td>vis_km</td><td>last updated time</td></tr>
+            {arr.map((weather, index) =>
+                <tr id={index}>
+                    <td><Link to={`/user/${params}/${weather.location.locationName}`}>{weather.location.locationName}</Link></td>
+                    <td>{weather.temp_c}</td>
+                    <td>{weather.wind_kph}</td>
+                    <td>{weather.wind_dir}</td>
+                    <td>{weather.humidity}</td>
+                    <td>{weather.precip_mm}</td>
+                    <td>{weather.vis_km}</td>
+                    <td>{weather.time}</td></tr>
 
-                )}
+            )}
 
-            </table>
-            <Routes>
-                <Route path="/:location" element={<EachLocation username={params} />} />
-            </Routes>
-        </>
+        </table></>
     );
 }
