32f637dc94248e223838f75b7645f15beba5d08a
pm-ep <105105579+pm-ep@users.noreply.github.com>
2022-05-13 18:47:09 +0800

    Replace marker by info-window

--- client/src/Client/MapView.js
+++ client/src/Client/MapView.js
@@ -1,7 +1,7 @@
 //Map view of location (User action #2)
 
 import React, { useEffect, useState } from "react";
-import { GoogleMap, useLoadScript, Marker, InfoBox } from '@react-google-maps/api';
+import { GoogleMap, useLoadScript, Marker, InfoWindow } from '@react-google-maps/api';
 import { Route, Routes, Link, useNavigate, Navigate, useParams } from 'react-router-dom';
 import EachLocation from "./EachLocation";
 
@@ -12,7 +12,7 @@ const mapContainerStyle = {
 };
 
 export default function MapView() {
-   //update the newest weather location before getting the weather
+    //update the newest weather location before getting the weather
 
 
 
@@ -28,56 +28,38 @@ export default function MapView() {
 function Map() {
     const params = useParams().username;
     const [arr, setarr] = useState([]);
-const centre = {
-    lat: 0,
-    lng: 0
-};
+    const centre = {
+        lat: 0,
+        lng: 0
+    };
 
-const clickupdate = function(){
-    fet();
-    window.alert("Location updated!")
-}
+    const clickupdate = function () {
+        fet();
+        window.alert("Location updated!")
+    }
 
-const fet = async () => {
-    const response = await fetch(`http://localhost:8080/weather`);
-    const res = await response.json();
-    setarr(res);
-}
+    const fet = async () => {
+        const response = await fetch(`http://localhost:8080/weather`);
+        const res = await response.json();
+        setarr(res);
+    }
 
 
-    useEffect(() => {fet();
+    useEffect(() => {
+        fet();
     }, []);
     console.log(arr)
 
     return (<><h2>Map View</h2>
         <GoogleMap zoom={2} center={centre} mapContainerStyle={mapContainerStyle}>
-            {arr.map((info, index) => <Mark lat={info.location.latitude} lng={info.location.longitude} name={info.location.locationName} params={params} />)}
-            {arr.map((info, index) => <IB lat={info.location.latitude} lng={info.location.longitude} name={info.location.locationName} params={params} />)}
+            {arr.map((info, index) => <IW lat={info.location.latitude} lng={info.location.longitude} name={info.location.locationName} params={params} />)}
         </GoogleMap>
-        <button  className="button" onClick={()=>clickupdate()}> Refresh time </button>
+        <button className="button" onClick={() => clickupdate()}>Refresh</button>
 
     </>)
 }
 
-
-class Mark extends React.Component {
-    constructor(props) {
-        super(props);
-    }
-
-    render() {
-        let name = this.props.name;
-        const lati = this.props.lat;
-        const long = this.props.lng;
-        const loc = { lat: lati, lng: long };
-        return (<Marker
-            title={name}
-            position={loc} />
-
-        )
-    }
-}
-class IB extends React.Component {
+class IW extends React.Component {
     constructor(props) {
         super(props);
     }
@@ -86,7 +68,7 @@ class IB extends React.Component {
         const lati = this.props.lat;
         const long = this.props.lng;
         const loc = { lat: lati, lng: long };
-        const params=this.props.params;
-                return <InfoBox position={loc}><Link to={`/user/${params}/${name}`}>{name}</Link></InfoBox>
+        const params = this.props.params;
+        return <InfoWindow position={loc}><Link to={`/user/${params}/${name}`}>{name}</Link></InfoWindow>
     }
 }
