7b527780200b72f135afaa15d45ec4b376e75dcf
Lwcxd <105037888+Lwcxd@users.noreply.github.com>
2022-05-14 15:30:22 +0800

    Update Search.js

--- client/src/Client/Search.js
+++ client/src/Client/Search.js
@@ -1,3 +1,12 @@
+/*
+Hui Cheuk Hin 1155124935
+Law Wan Chun Jeffrey 1155125137
+Lee Kam Chung Gordon 1155142809
+Leung Tsz Hin Leo 1155127195
+Wong Kai Lok Peter 1155125720
+Wong Yuen Ying Angel 1155125283
+*/
+
 //Search for keywords (User action #3)
 
 import React from "react";
