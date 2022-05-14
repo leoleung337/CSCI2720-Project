9c19726de308c97749d1fe47cfa8e79cbed51364
Lwcxd <105037888+Lwcxd@users.noreply.github.com>
2022-05-14 15:30:51 +0800

    Update RegisterPage.js

--- client/src/Login/RegisterPage.js
+++ client/src/Login/RegisterPage.js
@@ -1,4 +1,13 @@
 /*
+Hui Cheuk Hin 1155124935
+Law Wan Chun Jeffrey 1155125137
+Lee Kam Chung Gordon 1155142809
+Leung Tsz Hin Leo 1155127195
+Wong Kai Lok Peter 1155125720
+Wong Yuen Ying Angel 1155125283
+*/
+
+/*
 This js is for registration
  
 */
